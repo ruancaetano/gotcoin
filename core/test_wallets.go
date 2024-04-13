2db8c288950d193b79316409da3d857ac126766e
Ruan Caetano <ruansouza_caetano@hotmail.com>
2024-04-13 11:49:58 -0300

    feat: add test blockchain setup

---
+++ core/test_wallets.go
@@ -0,0 +1,20 @@
+package core
+
+var (
+	Wallet1 = &Wallet{
+		PrivateKey: "921e65937fad101d4d6d10caf748233ac252019d089797f97f5f6716109328ab",
+		PublicKey:  "0207565395aac81124bd5fe322799399f6713e8acfcdc1fce6165d7e9c525b15f6",
+	}
+	Wallet2 = &Wallet{
+		PrivateKey: "df1c058be0fe96a5cc2709a70446dcf904b9a9d19149aa47dc4b7ab426f63f7b",
+		PublicKey:  "03c7f55c3e0068394227f79e88918309891d26183161c8418d3bcdfec3746f62cb",
+	}
+	Wallet3 = &Wallet{
+		PrivateKey: "d4136f0dd288e74bccb8f4d004fd19a6ebc48eca5b995c946c0f74ff6c7701fa",
+		PublicKey:  "030f49c14b310fa98886f154cfe3dc566ec784e27603a15706e79a482227ce9097",
+	}
+	Miner1 = &Wallet{
+		PrivateKey: "f1b0fa47704607f96787e950a6a87d5a1f8cd5b6c54cd53c1b64186345250f7c",
+		PublicKey:  "0277cd93a4020fd2ef0c9bb1d070f403ee4d33c36d59114119bef238c646b6a2a5",
+	}
+)
