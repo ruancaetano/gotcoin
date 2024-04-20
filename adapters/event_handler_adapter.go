f540a03ccc48dd9410bc29359a1564f5ebec49ac
Ruan Caetano <ruansouza_caetano@hotmail.com>
2024-04-20 15:07:59 -0300

    feat: event handler adapter

---
+++ adapters/event_handler_adapter.go
@@ -0,0 +1,72 @@
+package adapters
+
+import (
+	"log"
+
+	"github.com/ruancaetano/gotcoin/core/events"
+	"github.com/ruancaetano/gotcoin/core/protocols"
+)
+
+type EventHandlerAdapter struct {
+	blockchainService protocols.BlockchainService
+}
+
+func NewEventHandlerAdapter(blockchainService protocols.BlockchainService) protocols.EventHandler {
+	return &EventHandlerAdapter{
+		blockchainService: blockchainService,
+	}
+}
+
+func (eh *EventHandlerAdapter) Handle(event events.EventData) {
+	switch event.Type {
+	case events.RequestBlockChainSyncEventType:
+		eh.handleBlockChainSyncRequest(event)
+		break
+	case events.ResponseBlockChainSyncEventType:
+		eh.handleBlockChainSyncResponse(event)
+		break
+	case events.NewTransactionEventType:
+		eh.handleNewTransaction(event)
+	case events.NewBlockEventType:
+		eh.handleNewBlock(event)
+	default:
+		log.Println("Unknown event type")
+	}
+}
+
+func (eh *EventHandlerAdapter) handleBlockChainSyncRequest(event events.EventData) {
+	if event.Metadata.OriginPeerID == "" {
+		log.Println("OriginPeerID is required")
+		return
+	}
+	eh.blockchainService.SendSyncBlocks(event.Metadata.OriginPeerID)
+}
+
+func (eh *EventHandlerAdapter) handleBlockChainSyncResponse(event events.EventData) {
+	if event.Block == nil {
+		log.Println("Block is required")
+		return
+	}
+
+	eh.blockchainService.ReceiveSyncBlock(event.Block, *event.BlockCount)
+}
+
+func (eh *EventHandlerAdapter) handleNewTransaction(event events.EventData) {
+	if event.Transaction == nil {
+		log.Println("Transaction is required")
+		return
+	}
+
+	if err := eh.blockchainService.AddTransaction(event.Transaction); err != nil {
+		log.Println("Skipping transaction: ", err.Error())
+	}
+}
+
+func (eh *EventHandlerAdapter) handleNewBlock(event events.EventData) {
+	if event.Block == nil {
+		log.Println("Block is required")
+		return
+	}
+
+	eh.blockchainService.AddBlock(event.Block, *event.NewDifficult)
+}
